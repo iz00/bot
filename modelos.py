62a24030b9763d0b27329022487a81eb14017238
iz00 <isadoracontsost@gmail.com>
2024-07-10 14:00:26 -0300

    Atualizar lista de modelos com modelos que oferecem Vale Mais - Troca Smart

--- modelos.py
+++ modelos.py
@@ -1,13 +1,19 @@
-"""Definição do dicionário `MODELOS`, que armazena os modelos disponíveis para o bot.
+"""Definição do dicionário MODELOS, que armazena os modelos disponíveis para o bot.
 Cada chave corresponde ao nome do modelo que aparecerá ao usuário (não necessariamente o mesmo da loja).
 Cada valor corresponde ao link do modelo correspondente na Samsung Shop."""
 
 MODELOS = {
-    "Samsung Galaxy M15 5G": "https://shop.samsung.com/br/galaxy-m15/p",
-    "S6 Lite Wifi": "https://shop.samsung.com/br/galaxy-tab-s6-lite-wifi/p",
-    "S6 Lite 4G": "https://shop.samsung.com/br/galaxy-tab-s6-lite-4g/p",
-    "S9 FE Wifi": "https://shop.samsung.com/br/galaxy-tab-s9-fe/p",
-    "S9 FE 5G": "https://shop.samsung.com/br/galaxy-tab-s9-fe-5g/p",
-    "S9 FE+ Wifi": "https://shop.samsung.com/br/galaxy-tab-s9-fe-plus/p",
-    "S9 FE+ 5G": "https://shop.samsung.com/br/galaxy-tab-s9-fe-plus-5g/p",
+    "Galaxy M15 5G": "https://shop.samsung.com/br/galaxy-m15/p",
+    "Galaxy M55 5G": "https://shop.samsung.com/br/galaxy-m55/p",
+    "Galaxy A35 5G": "https://shop.samsung.com/br/galaxy-a35/p",
+    "Galaxy A55 5G": "https://shop.samsung.com/br/galaxy-a55/p",
+    "Galaxy S24": "https://shop.samsung.com/br/galaxy-s24/p",
+    "Galaxy S24+": "https://shop.samsung.com/br/galaxy-s24-plus/p",
+    "Galaxy S24 Ultra": "https://shop.samsung.com/br/galaxy-s24-ultra/p",
+    # "S6 Lite Wifi": "https://shop.samsung.com/br/galaxy-tab-s6-lite-wifi/p",
+    # "S6 Lite 4G": "https://shop.samsung.com/br/galaxy-tab-s6-lite-4g/p",
+    # "S9 FE Wifi": "https://shop.samsung.com/br/galaxy-tab-s9-fe/p",
+    # "S9 FE 5G": "https://shop.samsung.com/br/galaxy-tab-s9-fe-5g/p",
+    # "S9 FE+ Wifi": "https://shop.samsung.com/br/galaxy-tab-s9-fe-plus/p",
+    # "S9 FE+ 5G": "https://shop.samsung.com/br/galaxy-tab-s9-fe-plus-5g/p",
 }
