cd6c9e2979d6d8e87097b9402bc0a2b1a63b2ef2
iz00 <isadoracontsost@gmail.com>
2024-07-25 02:02:12 -0300

    Utilizar polling como metodo de comunicacao

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@
 """
 Telegram bot que gera links para o carrinho da loja da Samsung com um produto escolhido e desconto do Vale Mais - Troca Smart.\n
 
-A comunicação do bot ocorre através de webhook com Flask, com base no [exemplo da documentação](https://docs.python-telegram-bot.org/en/v21.3/examples.customwebhookbot.html).\n
+A comunicação do bot ocorre através de polling.\n
 O bot funciona através do `ConversationHandler`, que define a ordem de algumas funções callback.\n
 A ordem das funções, definida nas etapas (`states`), pede ao usuário escolher o modelo, opcionalmente informar o link do modelo,
 a capacidade e a cor do modelo escolhido, e por fim a quantidade de links a serem gerados.\n
@@ -21,31 +21,23 @@ Envie /gerar para iniciar o processo de geração do link.
 Escolha um modelo (opcionalmente informe o link), a capacidade, a cor, e a quantidade de links.
 """
 
-import asyncio, logging, uvicorn
+import logging
 from modelos import MODELOS
 from utils import (
     gerar_link,
     informacoes_modelo,
 )
-from asgiref.wsgi import WsgiToAsgi
-from dataclasses import dataclass
-from flask import Flask, Response, abort, make_response, request
-from http import HTTPStatus
 from os import getenv
 from re import escape
 from telegram import ChatMember, InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.constants import ParseMode
 from telegram.error import TelegramError
 from telegram.ext import (
     Application,
-    CallbackContext,
     CallbackQueryHandler,
     CommandHandler,
     ContextTypes,
     ConversationHandler,
-    ExtBot,
     MessageHandler,
-    TypeHandler,
     filters,
 )
 
@@ -53,14 +45,10 @@ from telegram.ext import (
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-# Definições para o webhook
-URL = getenv("URL")
-PORT = int(getenv("PORT"))
-
-# ID do chat do admin no Telegram e TOKEN do bot gerado pelo BotFather
-ADMIN_CHAT_ID = getenv("ADMIN_CHAT_ID")
+# TOKEN do bot gerado pelo BotFather
 TOKEN = getenv("TOKEN")
 
 # ID do grupo para restrição de acesso
@@ -73,32 +61,12 @@ MODELO, LINK, CAPACIDADE, COR, QUANTIDADE = range(5)
 QUANTIDADE_LINKS = [1, 2, 3, 5, 10, 15, 20]
 
 
-@dataclass
-class WebhookUpdate:
-    """Dataclass simples para fazer wrap em um tipo customizado de update."""
-
-    user_id: int
-    payload: str
-
-
-class CustomContext(CallbackContext[ExtBot, dict, dict, dict]):
-    """Classe CallbackContext customizada que faz `user_data` disponível para updates do tipo `WebhookUpdate`."""
-
-    @classmethod
-    def from_update(
-        cls,
-        update: object,
-        application: "Application",
-    ) -> "CustomContext":
-        if isinstance(update, WebhookUpdate):
-            return cls(application=application, user_id=update.user_id)
-        return super().from_update(update, application)
-
-
 def restringir_acesso(func):
     """Restringir acesso aos comandos do bot apenas a usuários em determinado grupo."""
 
-    async def wrapper(update: Update, context: CustomContext, *args, **kwargs):
+    async def wrapper(
+        update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs
+    ):
         usuario_id = update.effective_user.id
 
         # Tenta pegar o `ChatMember` do usuário no grupo
@@ -127,7 +95,7 @@ def restringir_acesso(func):
 
 
 @restringir_acesso
-async def start(update: Update, context: CustomContext) -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Envia a mensagem com instruções quando o comando /start é enviado pelo usuário."""
     await update.message.reply_text(
         "Use o comando /gerar para iniciar a geração do link do carrinho."
@@ -135,7 +103,7 @@ async def start(update: Update, context: CustomContext) -> None:
 
 
 @restringir_acesso
-async def escolha_modelo(update: Update, context: CustomContext) -> int:
+async def escolha_modelo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Inicia o `ConversationHandler` e solicita ao usuário escolher um modelo."""
     # Monta botões com as opções de modelos do dicionário `MODELOS` e uma opção Outro
     teclado = [
@@ -156,7 +124,7 @@ async def escolha_modelo(update: Update, context: CustomContext) -> int:
     return MODELO
 
 
-async def informa_link(update: Update, context: CustomContext) -> int:
+async def informa_link(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Solicita ao usuário informar o link do modelo na Samsung Shop."""
     query = update.callback_query
     await query.answer()
@@ -174,7 +142,7 @@ async def informa_link(update: Update, context: CustomContext) -> int:
     return LINK
 
 
-async def escolha_capacidade(update: Update, context: CustomContext) -> int:
+async def escolha_capacidade(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Salva o modelo escolhido e solicita ao usuário escolher a capacidade do modelo."""
     query = update.callback_query
 
@@ -239,7 +207,7 @@ async def escolha_capacidade(update: Update, context: CustomContext) -> int:
     return CAPACIDADE
 
 
-async def escolha_cor(update: Update, context: CustomContext) -> int:
+async def escolha_cor(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Salva a capacidade escolhida e solicita ao usuário escolher a cor do modelo."""
     query = update.callback_query
     await query.answer()
@@ -259,7 +227,7 @@ async def escolha_cor(update: Update, context: CustomContext) -> int:
     return COR
 
 
-async def escolha_quantidade(update: Update, context: CustomContext) -> int:
+async def escolha_quantidade(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Salva a cor escolhida e solicita ao usuário escolher uma quantidade de links a seren gerados."""
     query = update.callback_query
     await query.answer()
@@ -282,7 +250,7 @@ async def escolha_quantidade(update: Update, context: CustomContext) -> int:
     return QUANTIDADE
 
 
-async def envia_link(update: Update, context: CustomContext) -> int:
+async def envia_link(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Gera e envia ao usuário os links para o carrinho com o modelo e o desconto do Vale Mais - Troca Smart."""
     query = update.callback_query
     await query.answer()
@@ -320,36 +288,10 @@ async def envia_link(update: Update, context: CustomContext) -> int:
     return ConversationHandler.END
 
 
-async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
-    """Lida com updates customizados."""
-    chat_member = await context.bot.get_chat_member(
-        chat_id=update.user_id, user_id=update.user_id
-    )
-    payloads = context.user_data.setdefault("payloads", [])
-    payloads.append(update.payload)
-    combined_payloads = "</code>\n• <code>".join(payloads)
-    text = (
-        f"O usuário {chat_member.user.mention_html()} enviou um novo payload. "
-        f"Até agora o usuário enviou os seguintes payloads: \n\n• <code>{combined_payloads}</code>"
-    )
-    await context.bot.send_message(
-        chat_id=ADMIN_CHAT_ID, text=text, parse_mode=ParseMode.HTML
-    )
-
-
-async def main() -> None:
-    """Começa o bot. Cria a aplicação do bot e uma aplicação web para lidar com os requests."""
-    context_types = ContextTypes(context=CustomContext)
-
-    # Cria a `application` e passa o `TOKEN` do bot
-    # `Updater` é None porque o webhook customizado que irá lidar com os updates
-    application = (
-        Application.builder()
-        .token(TOKEN)
-        .updater(None)
-        .context_types(context_types)
-        .build()
-    )
+def main() -> None:
+    """Começa o bot."""
+    # Cria a `application` e passa o TOKEN do bot
+    application = Application.builder().token(TOKEN).build()
 
     # Configurar `ConversationHandler` com as etapas (`states`)
     # Cada handler é associado a uma interação do usuário em resposta às solicitações do bot
@@ -382,68 +324,9 @@ async def main() -> None:
     # Adicionar `ConversationHandler` a `application` que vai ser utilizada para lidar com updates
     application.add_handler(conv_handler)
 
-    application.add_handler(TypeHandler(type=WebhookUpdate, callback=webhook_update))
-
-    # Passar as configurações do webhook para o Telegram
-    await application.bot.set_webhook(
-        url=f"{URL}/telegram", allowed_updates=Update.ALL_TYPES
-    )
-
-    # Configurar webserver
-    flask_app = Flask(__name__)
-
-    @flask_app.post("/telegram")  # type: ignore[misc]
-    async def telegram() -> Response:
-        """Lidar com updates vindos do Telegram colocando-os na `update_queue`."""
-        await application.update_queue.put(
-            Update.de_json(data=request.json, bot=application.bot)
-        )
-        return Response(status=HTTPStatus.OK)
-
-    @flask_app.route("/submitpayload", methods=["GET", "POST"])  # type: ignore[misc]
-    async def custom_updates() -> Response:
-        """
-        Lidar com updates do webhook também colocando-os na `update_queue`, se os parâmetros
-        requeridos foram passados corretamente.
-        """
-        try:
-            user_id = int(request.args["user_id"])
-            payload = request.args["payload"]
-        except KeyError:
-            abort(
-                HTTPStatus.BAD_REQUEST,
-                "Por favor passe ambos `user_id` e `payload` como parâmetros.",
-            )
-        except ValueError:
-            abort(HTTPStatus.BAD_REQUEST, "`user_id` deve ser uma string!")
-
-        await application.update_queue.put(
-            WebhookUpdate(user_id=user_id, payload=payload)
-        )
-        return Response(status=HTTPStatus.OK)
-
-    @flask_app.get("/healthcheck")  # type: ignore[misc]
-    async def health() -> Response:
-        """Para o endpoint health, responder apenas com uma mensagem de texto simples."""
-        response = make_response("O bot ainda está funcionando bem :)", HTTPStatus.OK)
-        response.mimetype = "text/plain"
-        return response
-
-    webserver = uvicorn.Server(
-        config=uvicorn.Config(
-            app=WsgiToAsgi(flask_app),
-            port=PORT,
-            use_colors=False,
-            host="0.0.0.0",
-        )
-    )
-
-    # Executar a application e o webserver juntos
-    async with application:
-        await application.start()
-        await webserver.serve()
-        await application.stop()
+    # Bot irá operar até usuário pressionar `Ctrl + C`
+    application.run_polling(allowed_updates=Update.ALL_TYPES, poll_interval=3)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
